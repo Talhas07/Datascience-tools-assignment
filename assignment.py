5f54bbc57f2b3c8f60733d4d54bbe4dab1b940c6
Talhas07 <137061576+Talhas07@users.noreply.github.com>
2023-07-22 18:34:21 +0500

    Add files via upload

---
+++ assignment.py
@@ -0,0 +1,135 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Data science tools Assignment 
+# 
+# 
+# 
+# 
+# 
+# 
+
+# ***
+
+# ## Introduction:
+# 1. This is the introduction for the assignment
+# 2. performed all exercises
+# 3. upload them
+# 
+
+# ***
+# 
+# 
+
+# ### Datascience Languages are...
+# 1. python
+# 2. Scala
+# 3. R
+
+# ***
+
+# ### Data science Libraries
+# 
+# * Pandas
+# * NumPy
+# * SciPy
+# * Machine Learning
+# * Scikit-learn
+# * TensorFlow
+# * Keras
+# 
+# 
+# 
+
+# ***
+# 
+
+# ### Datascience tools
+# 
+# 
+# | Category        | Tool           |
+# |-----------------|----------------|
+# | Data Analysis   | Pandas, NumPy, SciPy |
+# | Machine Learning| Scikit-learn, TensorFlow, Keras, PyTorch |
+# | Visualization   | Matplotlib, Seaborn, Plotly, Bokeh |
+# | NLP             | NLTK, SpaCy, Gensim |
+# | Data Visualization| Altair, ggplot, Folium |
+# | Big Data        | Dask, Apache Spark |
+# 
+# 
+
+# ***
+
+# ### Below are some examples of arithmetic expressions:
+# 
+# Addition:
+# 
+# Example: 5 + 3
+# Result: 8
+# Subtraction:
+# 
+# Example: 10 - 4
+# Result: 6
+# Multiplication:
+# 
+# Example: 6 * 7
+# Result: 42
+# Division:
+# 
+# Example: 15 / 3
+# Result: 5
+
+# In[5]:
+
+
+
+num1 = 5
+num2 = 3
+
+
+result_multiply = num1 * num2
+result_add = num1 + num2
+
+
+print("Result of multiplication:", result_multiply)
+print("Result of addition:", result_add)
+
+
+# ***
+
+# In[7]:
+
+
+
+def minutes_to_hours(minutes):
+    hours = minutes / 60
+    return hours
+
+
+
+minutes = 150
+hours_result = minutes_to_hours(minutes)
+print(f"{minutes} minutes is equal to {hours_result:.2f} hours")
+
+
+# ***
+
+# Objectives:
+# The objectives of this project are as follows:
+# 
+# * Data Collection: Gather relevant data from various sources to be used for analysis and modeling.
+# 
+# * Data Preprocessing: Clean and prepare the data for analysis, handling missing values, outliers, and data transformations.
+# 
+# * Exploratory Data Analysis (EDA): Perform exploratory analysis to gain insights, visualize patterns, and understand the relationships between variables.
+
+# ***
+
+# # Author's Name
+# ### TALHA 
+
+# In[ ]:
+
+
+
+
